6eb8c3697ed36d52bca5a057066995dd751ff487
ebel-c <cebel2@uoregon.edu>
2020-10-29 16:02:43 -0500

    explore total cover data 1999-2013

--- KLEE_data_cleaning.R
+++ KLEE_data_cleaning.R
@@ -2,3 +2,21 @@
 ### KLEE Data Cleaning
 ###########################################################
 
+## Load packages
+library("tidyverse")
+
+source("google-drive-function.R")
+
+## Read in Data
+#total cover by time, years 1999 - 2013
+totcov <- read_csv_gdrive("1nNVOMwaYY8GD9pYuLrXJro1aLR3RKn73") %>%
+  tbl_df()
+
+#rename columns
+names(totcov) <- c("Block",  "Treatment", "Cattle",   "Mega",     "Meso",     "199909",   "200002",   "200006",   "200101",  
+                   "200105",   "200202",   "200206",   "200302",   "200306",   "200402",   "200406",   "200502",   "200506",
+                   "200603",   "200606",   "200702",   "200706",   "200802",   "200806",   "200902",   "200906",   "201001",
+                   "201006",   "201106",   "201206",   "201306")
+
+#change data to long format for ease of graphing?
+totcov_long <- pivot_longer(totcov, cols = "199909":"201306", names_to = "Date", values_to = "Tot_Cover")
---
+++ KLEE_grazing_stability.R
@@ -0,0 +1,32 @@
+source("KLEE_data_cleaning.R")
+library(tidyverse)
+
+## Get data ready to graph as a time series
+meancov <- totcov_long %>%
+  group_by(Treatment, Date) %>% #group by treatment & date
+  summarize(Mean_Cov = mean(Tot_Cover)) #calculate the mean total cover at each treatment & date
+
+
+#change variable formats to get this to work in geom_line
+meancov$Treatment <- as.character(meancov$Treatment)
+meancov$Date <- as.numeric(meancov$Date)
+
+## Graph! 
+#graph the time series - all treatments in one plot
+ggplot(meancov, aes(x=Date)) +
+  geom_line(aes(y=Mean_Cov, col=Treatment)) +
+  theme_bw() +
+  ylab("Total Cover") + xlab("Date (yyyymm)")
+ 
+#separate by treatment to more easily view overlapping lines
+ggplot(meancov, aes(x=Date)) +
+  geom_line(aes(y=Mean_Cov, col=Treatment)) +
+  theme_bw() +
+  ylab("Total Cover") + xlab("Date (yyyymm)") +
+  facet_wrap(~Treatment)
+
+#graph as scatterplot
+ggplot(meancov, aes(x=Date, y=Mean_Cov, col=Treatment)) + 
+  geom_point() +
+  facet_wrap(~Treatment) +
+  theme_bw()
