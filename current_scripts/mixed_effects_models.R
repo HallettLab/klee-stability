ac9fe3c79791dcb5d07e2156742e7e2c6869dc6e
ebel-c <cebel2@uoregon.edu>
2022-05-13 19:14:34 -0700

    model diagnostics

--- current_scripts/mixed_effects_models.R
+++ current_scripts/mixed_effects_models.R
@@ -27,57 +27,26 @@ summary(fitsth_best)
 dev.off()
 #Default plot of standardised residuals versus fitted
 plot(fitsth_best, resid(., scaled=TRUE) ~ fitted(.), abline = 0,pch=16, xlab="Fitted values", ylab="Standardised residuals")
+## no trend of the residuals going in one direction
 ## the points are concentrated at the ends of the figure... why??
 ## could this be a result of the categorical variable for herbivory?
 ## does the variance look constant across the fitted range?
 
 ## this is checking for heteroscedasticity: whether the standard deviations of a predicted variable, monitored over different values of the independent variable are non-constant 
 
-#Plotting the residuals against each explanatory variable
-plot(fitsth_best, resid(., scaled=TRUE) ~ cows, abline = 0,pch=16,xlab="Cows",ylab="Standardised residuals")
-## this didn't work, covariate needs to be numeric. hmm. 
-
-ggplot(data.frame(x1=stability_mechanisms$cows,pearson=residuals(fitsth_best,type="pearson")),
-       aes(x=x1,y=pearson)) +
-  geom_point() +
-  theme_bw()
-
 ## Normality of Residuals
 dev.off()
 qqnorm(resid(fitsth_best))
 qqline(resid(fitsth_best))
 ## it looks like there are deviations from the line... 
+## it would be concerning if there were a curve
 
 #We can use a default plot of the standardised residuals versus fitted 
 #But add | <random effect> to see it per level of the random effect
 plot(fitsth_best, resid(., scaled=TRUE) ~ fitted(.)| BLOCK, abline = 0,pch=16,xlab="Fitted values",ylab="Standardised residuals")
 
-#Calculate leverage
-lev<-hat(model.matrix(fitsth_best))
 
-#Plot leverage against standardised residuals
-plot(resid(fitsth_best,type="pearson")~lev,las=1,ylab="Standardised residuals",xlab="Leverage")
-
-
-#Calculate Cook's Distance
-cd<-cooks.distance(fitsth_best)
-#N.B. If Cook's distance is greater than 1 this highlights problematic datapoints
-
-#Plot leverage and Cook's distance together
-par(mfrow=c(1,1))
-plot(lev,pch=16,col="red",ylim=c(0,1.2),las=1,ylab="Leverage/Cook's distance value")
-points(cd,pch=17,col="blue")
-points(x=150,y=1.1,pch=16,col="red")
-points(x=150,y=0.9,pch=17,col="blue")
-text(x=155,y=1.1,"Leverage",adj=c(0,0.5))
-text(x=155,y=0.9,"Cook's distance",adj=c(0,0.5))
-
-
-hist(as.vector(unlist(ranef(fitsth_best)$BLOCK)),breaks=seq(-2,1,0.25),border=NA)
-
-lattice::dotplot(ranef(fitsth_best, condVar=TRUE))
-
-
+## Alternative models
 fitsth_top_alt <- lmer(stability~cows + wildlife + (1|BLOCK), data = stability_mechanisms, na.action = "na.fail")
 summary(fitsth_top_alt)
 
@@ -101,14 +70,15 @@ dev.off()
 #Default plot of standardised residuals versus fitted
 plot(fitdrst_best, resid(., scaled=TRUE) ~ fitted(.), abline = 0,pch=16, xlab="Fitted values", ylab="Standardised residuals")
 
-#Plotting the residuals against each explanatory variable
-plot(fitdrst_best, resid(., scaled=TRUE) ~ Dscore, abline = 0,pch=16,xlab="Drought Score",ylab="Standardised residuals")
-
 ## Normality of Residuals
 dev.off()
 qqnorm(resid(fitdrst_best))
 qqline(resid(fitdrst_best))
 
+#Plotting the residuals against each explanatory variable
+plot(fitdrst_best, resid(., scaled=TRUE) ~ Dscore, abline = 0,pch=16,xlab="Drought Score",ylab="Standardised residuals")
+
+## Alternative model: 
 fitdrst_best_alt <- lmer(stability~Dscore+cows+wildlife+mega + (1|BLOCK) + (1|Unique_ID), data = dmw10, na.action = "na.fail")
 summary(fitdrst_best_alt)
 
@@ -123,6 +93,17 @@ fitcvr_best <- lmer(classicVR~cows+mega + (1|BLOCK), data = stability_mechanisms
 summary(fitcvr_best)
 
 
+## model diagnostics
+dev.off()
+#Default plot of standardised residuals versus fitted
+plot(fitcvr_best, resid(., scaled=TRUE) ~ fitted(.), abline = 0,pch=16, xlab="Fitted values", ylab="Standardised residuals")
+
+## Normality of Residuals
+dev.off()
+qqnorm(resid(fitcvr_best))
+qqline(resid(fitcvr_best))
+
+
 ## MODEL FOR FIGURE 4B ## 
 ## how is the variance ratio (10 year) predicted by drought & herbivory?
 fitdrcvr <- lmer(classicVR~Dscore+cows+wildlife+mega + (1|BLOCK) + (1|Unique_ID), data = dmw10, na.action = "na.fail")
@@ -131,7 +112,17 @@ dredge(fitdrcvr)
 fitdrcvr_best <- lmer(classicVR~Dscore+cows+wildlife + (1|BLOCK) + (1|Unique_ID), data = dmw10, na.action = "na.fail")
 summary(fitdrcvr_best)
 
-plot(fitdrcvr_best)
+## model diagnostics
+dev.off()
+#Default plot of standardised residuals versus fitted
+plot(fitdrcvr_best, resid(., scaled=TRUE) ~ fitted(.), abline = 0,pch=16, xlab="Fitted values", ylab="Standardised residuals")
+
+## Normality of Residuals
+dev.off()
+qqnorm(resid(fitdrcvr_best))
+qqline(resid(fitdrcvr_best))
+
+
 
 ## MODEL FOR FIGURE 4C ##
 ## how is dominant species population stability predicted by varying groups of herbivores?
@@ -140,6 +131,16 @@ dredge(fitdpsh)
 
 fitdpsh_best <- lmer(meanpopstab~1 + (1|BLOCK), data = stability_mechanisms, na.action = "na.fail")
 summary(fitdpsh_best)
+
+## model diagnostics
+dev.off()
+#Default plot of standardised residuals versus fitted
+plot(fitdpsh_best, resid(., scaled=TRUE) ~ fitted(.), abline = 0,pch=16, xlab="Fitted values", ylab="Standardised residuals")
+
+## Normality of Residuals
+dev.off()
+qqnorm(resid(fitdpsh_best))
+qqline(resid(fitdpsh_best))
 
 
 ## MODEL FOR FIGURE 4D ## 
@@ -151,6 +152,16 @@ dredge(fitdrps)
 fitdrps_best <- lmer(mean_popst~cows + (1|BLOCK) + (1|Unique_ID), data = dmw10, na.action = "na.fail")
 summary(fitdrps_best)
 ## CODE EDIT: Again, the model with just cows falls within <2 delta AIC of the best model, so that should be acknowledged in text.
+
+## model diagnostics
+dev.off()
+#Default plot of standardised residuals versus fitted
+plot(fitdrps_best, resid(., scaled=TRUE) ~ fitted(.), abline = 0,pch=16, xlab="Fitted values", ylab="Standardised residuals")
+
+## Normality of Residuals
+dev.off()
+qqnorm(resid(fitdrps_best))
+qqline(resid(fitdrps_best))
 
 fitdrpstop <- lmer(mean_popst~cows+wildlife + (1|BLOCK) + (1|Unique_ID), data = dmw10, na.action = "na.fail")
 summary(fitdrpstop)
@@ -171,6 +182,19 @@ summary(fitrih_best)
 ## this one has singularity issues... not sure what to do... 
 ## CODE EDIT: Re-run as a linear model without the random effect, you should get essentially the same answer because the data here aren't supporting the complexity of the random effects structure so it's estimating 0 random effects variance.
 
+## model diagnostics
+dev.off()
+#Default plot of standardised residuals versus fitted
+plot(fitrih_best, resid(., scaled=TRUE) ~ fitted(.), abline = 0,pch=16, xlab="Fitted values", ylab="Standardised residuals")
+plot(fitrih_best)
+
+## Normality of Residuals
+dev.off()
+qqnorm(resid(fitrih_best))
+qqline(resid(fitrih_best))
+
+
+
 ## MODEL FOR FIGURE 4F ##
 ## how is richness (10 year) predicted by drought and herbivory?
 fitdrri <- lmer(richness~Dscore+cows+wildlife+mega + (1|BLOCK) + (1|Unique_ID), data = dmw10, na.action = "na.fail")
@@ -180,6 +204,19 @@ fitdrri_best <- lmer(richness~Dscore+wildlife + (1|BLOCK) + (1|Unique_ID), data 
 summary(fitdrri_best)
 ## this one has singularity issues... not sure what to do... 
 ## CODE EDIT: see suggestion above
+
+## model diagnostics
+dev.off()
+#Default plot of standardised residuals versus fitted
+plot(fitdrri_best, resid(., scaled=TRUE) ~ fitted(.), abline = 0,pch=16, xlab="Fitted values", ylab="Standardised residuals")
+plot(fitdrri_best)
+
+## Normality of Residuals
+dev.off()
+qqnorm(resid(fitdrri_best))
+qqline(resid(fitdrri_best))
+
+
 
 fitdrritop <- lmer(richness~Dscore+wildlife+mega + (1|BLOCK) + (1|Unique_ID), data = dmw10, na.action = "na.fail")
 summary(fitdrritop)
@@ -204,13 +241,13 @@ vif(fitstabmech)
 fitstabmech_best <- lmer(stability~classicVR + meanpopstab + (1|BLOCK), data = stability_mechanisms, na.action = "na.fail")
 summary(fitstabmech_best)
 
-## diagnostic plots
-ggplot(data.frame(x1=stability_mechanisms$classicVR,pearson=residuals(fitstabmech_best,type="pearson")),
-       aes(x=x1,y=pearson)) +
-  geom_point() +
-  theme_bw()
+## model diagnostics
+dev.off()
+#Default plot of standardised residuals versus fitted
+plot(fitstabmech_best, resid(., scaled=TRUE) ~ fitted(.), abline = 0,pch=16, xlab="Fitted values", ylab="Standardised residuals")
 
-ggplot(data.frame(x2=stability_mechanisms$meanpopstab,pearson=residuals(fitdrcvr_best,type="pearson")),
-       aes(x=x2,y=pearson)) +
-  geom_point() +
-  theme_bw()
+## Normality of Residuals
+dev.off()
+qqnorm(resid(fitstabmech_best))
+qqline(resid(fitstabmech_best))
+
